8de5106941f78723caa48e5d345b8a4ecc6738ad
Tamim <tammu1000@gmail.com>
2023-11-24 10:58:57 +0530

    requirements

--- demo_app.py
+++ demo_app.py
@@ -1,3 +1,36 @@
-name = input('What is your name? \n')
+import requests
+v = 1.0
+def check_for_update(current_version):
+    # Query GitHub API to get the latest release version
+    url = "https://api.github.com/repos/your_username/your_repository/releases/latest"
+    response = requests.get(url)
+    latest_version = response.json()["tag_name"]
 
-print(f"\nWow {name} really sucks.")
+    # Compare versions and return True if an update is available
+    return latest_version > current_version
+
+
+while True:
+
+    check_for_update(v)
+
+    name = input('What is your name? \n')
+
+
+    if name.lower() == 'exit':
+        exit()
+
+    else:
+        print(f"\nWow {name} really sucks.\n")
+
+
+
+
+
+
+''' 
+Command to run:
+
+pyinstaller --noconfirm --onedir --console --name "ci_cd_test"  "D:/PROJECTS/ci_cd_implementation/demo_app.py" 
+
+'''
